7b92107b78335cccb9fa82c6fc883528751c279e
Sohaib Ahmed <sohaibahmed.hypersoft@gmail.com>
2025-03-04 11:17:35 +0500

    Initial Commit

--- app/src/main/java/dev/pegasus/swipeexoplayer/data/DataSourceVideos.kt
+++ app/src/main/java/dev/pegasus/swipeexoplayer/data/DataSourceVideos.kt
@@ -16,10 +16,18 @@ class DataSourceVideos {
     fun getList(): List<ItemVideo> {
         return listOf(
             ItemVideo(0, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
-            ItemVideo(1, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
-            ItemVideo(2, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
-            ItemVideo(3, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
-            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
+            ItemVideo(1, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"),
+            ItemVideo(2, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"),
+            ItemVideo(3, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerJoyrides.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4"),
+            ItemVideo(4, "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WhatCarCanYouGetForAGrand.mp4"),
         )
     }
 }
